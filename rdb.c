ec4b0a5933a11cfb5b5688931d88f4adc47b08cc
Your Name <you@example.com>
2023-12-04 14:59:42 +0530

    first commit

---
+++ rdb.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum Color { RED, BLACK };
+
+struct Node {
+    int data;
+    enum Color color;
+    struct Node *left, *right, *parent;
+};
+
+struct Node *root = NULL;
+
+
+void insertRBTree(int key);
+void fixInsertRBTree(struct Node *x);
+void rotateLeft(struct Node *x);
+void rotateRight(struct Node *x);
+void printRBTree(struct Node *root);
+
+int main() {er elements to insert into Red-Black tree(Enter -1 to stop):\n");
+    while(1){
+	scanf("%d",&key);
+	if(key==-1)
+		break;
+
+        insertRBTree(key);
+        printf("Inserted: %d\n", key);
+        printRBTree(root);
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+struct Node *createNode(int key) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = key;
+    newNode->color = RED;
+    newNode->left = newNode->right = newNode->parent = NULL;
+    return newNode;
+}
+void rotateLeft(struct Node *x) {
+    struct Node *y = x->right;
+    x->right = y->left;
+
+    if (y->left != NULL)
+        y->left->parent = x;
+
+    y->parent = x->parent;
+
+    if (x->parent == NULL)
+        root = y;
+    else if (x == x->parent->left)
+        x->parent->left = y;
+    else
+        x->parent->right = y;
+
+    y->left = x;
+    x->parent = y;
+}
+void rotateRight(struct Node *x) {
+    struct Node *y = x->left;
+    x->left = y->right;
+
+    if (y->right != NULL)
+        y->right->parent = x;
+
+    y->parent = x->parent;
+
+    if (x->parent == NULL)
+        root = y;
+    else if (x == x->parent->left)
+        x->parent->left = y;
+    else
+        x->parent->right = y;
+
+    y->right = x;
+    x->parent = y;
+}
+void fixInsertRBTree(struct Node *x) {
+    while (x != root && x->parent->color == RED) {
+        if (x->parent == x->parent->parent->left) {
+            struct Node *y = x->parent->parent->right;
+            if (y != NULL && y->color == RED) {
+                x->parent->color = BLACK;
+                y->color = BLACK;
+                x->parent->parent->color = RED;
+                x = x->parent->parent;
+            } else {
+                if (x == x->parent->right) {
+                    x = x->parent;
+                    rotateLeft(x);
+                }
+                x->parent->color = BLACK;
+                x->parent->parent->color = RED;
+                rotateRight(x->parent->parent);
+            }
+        } else {
+            struct Node *y = x->parent->parent->left;
+            if (y != NULL && y->color == RED) {
+                x->parent->color = BLACK;
+                y->color = BLACK;
+                x->parent->parent->color = RED;
+                x = x->parent->parent;
+            } else {
+                if (x == x->parent->left) {
+                    x = x->parent;
+                    rotateRight(x);
+                }
+                x->parent->color = BLACK;
+                x->parent->parent->color = RED;
+                rotateLeft(x->parent->parent);
+            }
+        }
+    }
+
+    root->color = BLACK;
+}
+void insertRBTree(int key) {
+    struct Node *newNode = createNode(key);
+    struct Node *y = NULL;
+    struct Node *x = root;
+
+    while (x != NULL) {
+        y = x;
+        if (newNode->data < x->data)
+            x = x->left;
+        else
+            x = x->right;
+    }
+
+    newNode->parent = y;
+
+    if (y == NULL)
+        root = newNode;
+    else if (newNode->data < y->data)
+        y->left = newNode;
+    else
+        y->right = newNode;
+
+    fixInsertRBTree(newNode);
+}
+void printRBTree(struct Node *root) {
+    if (root != NULL) {
+        printRBTree(root->left);
+        printf("(%d, %s) ", root->data, (root->color == RED) ? "RED" : "BLACK");
+        printRBTree(root->right);
+    }
+}
+    int key;
+    printf("Ent
